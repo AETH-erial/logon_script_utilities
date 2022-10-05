2c6a1c8bba4ee7bbfa5eac65b9f4259e12e49810
aeth <aetheriak@proton.me>
2022-10-05 17:29:44 -0500

    added more utilites

--- local_net_utilities.py
+++ local_net_utilities.py
@@ -5,7 +5,7 @@ import struct
 
 
 ##  method to get the default gateway of your machine
-def get_default_gateway():
+def get_default_gateway() -> str:
     """Method to get the default gateway of your machine.
     Linux/Unix based method.
     
@@ -17,23 +17,35 @@ def get_default_gateway():
             fields = line.strip().split()
             if fields[1] != '00000000' or not int(fields[3], 16) & 2:
                 continue
-            return ipaddress.IPv4Address(socket.inet_ntoa(struct.pack("<L", int(fields[2], 16))))
+            def_gateway = ipaddress.IPv4Address(socket.inet_ntoa(struct.pack("<L", int(fields[2], 16))))
+            test_result = f"""
+            Your machine appears to have a default gateway.
+            Address: {def_gateway}"""
+            return test_result
 
-def test_dns_resolution(url: str, port: int) -> bool:
+def test_dns_resolution(url: str) -> str:
     """Method to test if DNS resolves or not.
     
     :rtype: bool
     :returns: True or False based on if DNS is resolving or not.
     """
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        host_ip = socket.gethostbyname(url)
-    except socket.error as err:
-        raise Exception(f'Exception raised when attempting DNS resolution {err}') from err
-        sys.exit()
-    sock.connect((host_ip, port))
-
+        str_dns_addr = socket.gethostbyname(url)
+        dns_test_address = ipaddress.IPv4Address(str_dns_addr)
+        test_result = f"""
+            DNS resolution deemed successful.
+            '{url}' resolved to: {dns_test_address}"""
+        return test_result
+    except socket.error as error:
+        raise Exception(f'Exception raised when attempting DNS resolution {error}') from error
     
+def who_am_i() -> str:
+    """Method to return the address and hostname of the box.
     
-test_dns_resolution(url='www.google.com', port=80)
+    :rtype: str
+    :returns: hostname and local address
+    """
+    local_host = socket.gethostname()
+    hostname_resolution = socket.gethostbyname(local_host)
+    test_result = {local_host: hostname_resolution}
+    return test_result
