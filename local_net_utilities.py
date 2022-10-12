2e99519843e0c01d4729ebc267a886b6284fc461
aeth <aetheriak@proton.me>
2022-10-11 20:22:52 -0500

    added a couple new utilites and refactored the logon script

--- local_net_utilities.py
+++ local_net_utilities.py
@@ -1,7 +1,11 @@
 import ipaddress
+from ipaddress import IPv4Address
 import socket
 import sys
 import struct
+import subprocess
+from subprocess import Popen, PIPE
+from urllib.request import urlopen, HTTPError
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
@@ -19,7 +23,7 @@ def get_default_gateway() -> str:
             fields = line.strip().split()
             if fields[1] != '00000000' or not int(fields[3], 16) & 2:
                 continue
-            def_gateway = ipaddress.IPv4Address(socket.inet_ntoa(struct.pack("<L", int(fields[2], 16))))
+            def_gateway = IPv4Address(socket.inet_ntoa(struct.pack("<L", int(fields[2], 16))))
             test_result = {'default_gateway': def_gateway}
             return test_result
 
@@ -34,7 +38,7 @@ def test_dns_resolution(url: str) -> str:
     """
     try:
         str_dns_addr = socket.gethostbyname(url)
-        dns_test_address = ipaddress.IPv4Address(str_dns_addr)
+        dns_test_address = IPv4Address(str_dns_addr)
         test_result = {url: dns_test_address}
         return test_result
     except socket.error as error:
@@ -65,3 +69,91 @@ def get_time(timezone: str) -> datetime:
     return timestamp
 
 
+def get_external_ip() -> IPv4Address:
+    """Get the external facing IP address of the network.
+    
+    :type: None
+    :param: None
+    
+    :rtype: IPv4Address
+    :returns: IPv4Address class object of the external IP.
+    """
+    try:
+        with urlopen('https://ident.me') as response:
+            external_ip = response.read().decode('ascii')
+        test_result = {'External IP Address': external_ip}
+        return test_result
+    except urllib.error as error:
+        raise Exception(f'There was an issue getting your external IP. {error}')
+
+
+def is_valid_address(address: str) -> bool:
+    """checks to see if the address passed converts to bytes.
+    
+    :type address: str
+    :param address: the address to be tested
+    
+    :rtype: bool
+    :returns: true if the address is in valid format.
+    
+    :raises AttributeError: if the address is non-parseable.
+    """
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError as error:
+        raise AttributeError(f'Address was non-parseable. Raising from is_valid_address{error}')
+    return True
+
+def get_dns_server() -> IPv4Address:
+    """Get the IP address of the DNS server you are using.
+    
+    :type: None
+    :param: None
+    
+    :rtype: IPv4Address
+    :returns: An IPv4Address class object of your DNS Server's address.
+    """
+    dns_servers = []
+    test_results = {}
+    with open('/etc/resolv.conf') as fp:
+        for num, line in enumerate(fp):
+            columns = line.split()
+            if columns[0] == 'nameserver':
+                dns_entry = columns[1]
+                if is_valid_address(dns_entry) == True:
+                    dns_servers.append(dns_entry)
+    test_results['dns servers'] = dns_servers
+    return test_results
+
+def resolve_ip_to_fqdn(address: str) -> str:
+    """Resolve an IP address to an FQDN.
+    
+    :type address: str
+    :param address: the address to be resolved.
+    
+    :rtype: str
+    :returns: the FQDN of the address that was resolved.
+    
+    :raises Exception: if IP couldn't resolve to a hostname.
+    """
+    command_output = []
+    parsed_output = []
+    script_call = subprocess.Popen(
+                            ['dig',
+                            '+noall',
+                            '+authority',
+                            '-x',
+                            f'{address}'],
+                            stdout=PIPE,
+                            stderr=PIPE
+                            )
+    with script_call.stdout:
+        for output in script_call.stdout:
+            entry = output.decode('utf-8')
+            command_output.append(entry)
+        parsed_output = command_output[0].split()
+        SOA = parsed_output[0]
+    test_result = {
+        'SOA': f'{SOA}'
+    }
+    return test_result
--- logon_script.py
+++ logon_script.py
@@ -1,21 +1,33 @@
 import os
 import sys
 import subprocess
+import time
 from subprocess import Popen, PIPE
 from datetime import datetime
 from zoneinfo import ZoneInfo
 import pprint
-from local_net_utilities import who_am_i, test_dns_resolution, get_default_gateway, get_time
+from local_net_utilities import (who_am_i,
+                                test_dns_resolution,
+                                get_default_gateway,
+                                get_time,
+                                get_external_ip,
+                                get_dns_server,
+                                resolve_ip_to_fqdn)
+from console_colors import ConsoleColors
+
 
 session_logging = [
     get_time(timezone='America/Chicago'),
     who_am_i(),
     get_default_gateway(),
+    get_external_ip(),
+    get_dns_server(),
     test_dns_resolution(url='www.archlinux.org')
 ]
-for entry in session_logging:
-    for key, val in entry.items():
-        print(f"{key} :: {val}")
+for dns_servers in get_dns_server():
+    session_logging.append(resolve_ip_to_fqdn(dns_servers))
+
+
 script_call = subprocess.Popen(
     ['vpn_diag.sh'],stdout=PIPE,
     stderr=PIPE,
@@ -27,9 +39,8 @@ with script_call.stdout:
         sys.stdout.write(line)
         sys.stdout.flush()
 
-startx_call = subprocess.Popen(
-    ['startx'],
-    stdout=False,
-    stderr=False
-)
-startx_call.communicate()
+
+for entry in session_logging:
+    for key, val in entry.items():
+        print(f"{ConsoleColors.UNDERLINE}{key}{ConsoleColors.ENDC} :: {ConsoleColors.OKGREEN}{val}{ConsoleColors.ENDC}")
+        time.sleep(.5)
